40ec2f008e56bc8a6681145c5963530acdf7a4ea
Richard Davidsson <r-1chard@hotmail.com>
2022-11-09 14:19:33 +0100

    URLLesson tested

---
+++ src/UrlLEsson/URLDemo.java
@@ -0,0 +1,25 @@
+package UrlLEsson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class URLDemo {
+
+    public URLDemo() throws IOException {
+
+        URL url = new URL("https://studentportal.nackademin.se");
+
+        BufferedReader buf = new BufferedReader(new InputStreamReader(url.openStream()));
+        String temp;
+
+        while ((temp = buf.readLine()) != null){
+            System.out.println(temp);
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        URLDemo test = new URLDemo();
+    }
+}
